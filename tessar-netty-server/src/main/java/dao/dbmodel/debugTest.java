6d546102d5bb56f982d435b16092c62f55c73b63
Vova <nbabob520@163.com>
2017-12-21 17:07:17 +0800

    vova
    vova

--- tessar-netty-server/src/main/java/dao/dbmodel/debugTest.java
+++ tessar-netty-server/src/main/java/dao/dbmodel/debugTest.java
@@ -1,12 +1,16 @@
 package dao.dbmodel;
 
+import dao.mysql.UseMySql;
+import nettydemo.User;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class debugTest {
     @Test
-    public void fun(){
-
+    public void fun() throws IOException {
+        User user = new User("vova", 123);
+        UseMySql.insert(user);
     }
 }
